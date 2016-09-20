ca48b864f213d9b9800dfc3f8e6fcce4ae807bbd
0xbug <clinlee.me@gmail.com>
2016-09-20 19:03:59 +0800

    Update settings.py
    fixed csrf bug

--- SQLiScanner/settings.py
+++ SQLiScanner/settings.py
@@ -144,6 +144,12 @@ CORS_ORIGIN_ALLOW_ALL = True
 # REST_FRAMEWORK
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     # 'DATETIME_FORMAT': ("%m-%d %H:%M"),
